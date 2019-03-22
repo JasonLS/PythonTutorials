5b091b298fe09bd82e87aa34d409c9f222dd0b6d
JasonLS <jsedluk01@gmail.com>
2019-03-22 15:42:59 -0400

    Update TextToSpeech.py

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,17 +1,19 @@
+#Adapted from https://learn.adafruit.com/speech-synthesis-on-the-raspberry-pi/programs
+#install flite using sudo apt-get install flite
+
+
+
 import os
  
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
+RedButton = 7 #Variable "RedButton" is set to pin 7 on header
 
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(RedButton, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Sets Pin 7 as an input with pullup
  
 
 while 1:
-    if GPIO.input(7) == GPIO.HIGH:
-        os.system('flite -t "Use the force Luke!" ')
-
-    
+    if GPIO.input(RedButton) == GPIO.LOW: #If the button is pushed...
+        os.system('flite -t "Put " ') #Text to Speech Command
